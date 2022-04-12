8befab4132836ed1561b0fb2dcaf2be18bebd833
Gustavo00-dev <gustavorodrigues955@gmail.com>
2022-04-12 19:29:14 -0300

    Criado menu - + Classes adicionais.

---
+++ Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainOrder
+{
+    class Order
+    {
+        long entrada;
+
+        public Order()
+        {
+            entrada = 0;
+        }
+
+        public void Menu()
+        {
+            do
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("1 - Selection Sort");
+                Console.WriteLine("2 - Quick Sort");
+                Console.WriteLine("3 - Bubble Sort");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("-------------------------------");
+
+                entrada = long.Parse(Console.ReadLine());
+
+                this.CmdClear();
+
+                switch (entrada)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        break;
+                }
+            } while (entrada != 0);
+        }
+
+        public void CmdClear()
+        {
+            Console.Clear();
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -6,30 +6,9 @@ namespace MainOrder
     {
         static void Main(string[] args)
         {
-            long entrada;
-            do
-            {
-                Console.WriteLine("---------------------");
-                Console.WriteLine("1 - Selection Sort");
-                Console.WriteLine("2 - Quick Sort");
-                Console.WriteLine("3 - Bubble Sorte");
-                Console.WriteLine("0 -  Sair");
-                Console.WriteLine("---------------------");
+            Order o = new Order();
 
-                entrada = long.Parse(Console.ReadLine());
-
-                switch (entrada)
-                {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 0:
-                        break;
-                }
-            } while (entrada != 0);
+            o.Menu();
         }
     }
 }
